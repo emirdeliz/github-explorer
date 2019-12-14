82a2aa71d9011a3d718dc712a084bf7535a9bb7d
Emir Marques <emirdeliz@gmail.com>
2019-12-14 20:10:30 -0300

    feat: added the theme with colors and font-size

---
+++ system/theme.ts
@@ -0,0 +1,21 @@
+enum EColors {
+  navbarBackgroundColor    = "hsl(210, 12%, 16%)",
+  navColor                 = "hsl(0, 0%, 100%)",
+  logoGithub               = "hsl(0, 0%, 100%)",
+  loading                  = "hsl(210, 12%, 16%)",
+  searchIcon               = "hsl(210, 12%, 16%)",
+  dropdownSearchBorder     = "hsl(0, 0%, 0%)",
+  dropdownSearchBackground = "hsl(0, 0%, 100%)",
+  dropdownSearchMenu       = "hsl(0, 0%, 100%)",
+  dropdownSearchMenuHover  = "hsl(0, 0%, 87%)",
+  dropdownSearchMenuColor  = "hsl(0, 0%, 0%)",
+  dropdownSearchMenuLabel  = "hsl(212, 97%, 43%)",
+  dropdownSearchMenuDesc   = "hsl(212, 9%, 38%)",
+}
+
+enum EFontSize {
+  small = "0.8rem",
+  normal = "0.9rem",
+};
+
+export { EColors, EFontSize };
